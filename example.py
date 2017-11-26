acdf88efb2b636ab40f8ecb872bf033455a1ab5a
Christopher Jenness <christopherjenness@gmail.com>
2017-11-26 05:45:36 -0500

    Clean with flake8 and chaining

--- example.py
+++ example.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import forward_selection_viz
 
+
 def organize_data():
     """
     Load, organize, and normalize data
     """
-    df = pd.read_csv('data/2017.csv')
-    df.drop(['Arena', 'Rk', 'L', 'PL', 'PW', 'W', 'SOS', 'SRS', 'ORtg', 'DRtg', 'Attendance'],
-            1, inplace=True)
-    df = df.set_index('Team')
+    df = (pd.read_csv('data/2017.csv')
+          .drop(['Arena', 'Rk', 'L', 'PL', 'PW', 'W', 'SOS', 'SRS',
+                 'ORtg', 'DRtg', 'Attendance'],
+                1, inplace=True)
+          .set_index('Team'))
     y = df['MOV']
     df = (df - df.mean()) / df.std()
     X = df.drop('MOV', 1)
     return X, y
 
+
 if __name__ == '__main__':
     X, y = organize_data()
     forward_selection_viz.iterative_forward_select(X, y, depth=5)
-
-
